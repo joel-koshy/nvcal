0769b4004968206723c0c08f498d5e3cd97c872e
Joel <example@email.com>
2026-07-13 15:30:07 -0400

    (chore) fix route
    Moved sync folder into api/sync
    moved events into api/events

--- backend/src/routes/api/index.ts
+++ backend/src/routes/api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
-import eventsRouter from "../events";
+import eventsRouter from "./events";
 import type { Bindings } from "hono/types";
-import syncRouter from "../sync";
+import syncRouter from "./sync";
 const apiRouter = new Hono<{ Bindings: Bindings }>();
 apiRouter.route('/events', eventsRouter)
 apiRouter.route('/sync', syncRouter)
